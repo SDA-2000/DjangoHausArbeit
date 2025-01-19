7fba7e457d69875efc6317bcbd6efa5f7847797f
SDA-2000 <ssva832@gmail.com>
2025-01-20 02:56:36 +1000

    Commit for task4

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,11 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.get_endp, name = 'guest'),
-    path('me', views.get2_endp_crud_read),
+    path('', views.red),
     path('accounts/login/', views.login, name='login'),
-    path('accounts/registration', views.registration),
-    path('accounts/profile/<str:username>/', views.get2_endp_crud_read, name='profile'),
-    path('accounts/profile/<str:username>/change/<str:change_user>/', views.change_user_data, name='change_user_data'),
-    path('accounts/profile/<str:username>/delete/<str:del_user>/', views.delete_user, name='delete_user')
+    path('accounts/registration', views.registration, name='reg'),
+    path('accounts/profile/<str:username>/', views.profile, name='profile'),
 ]
